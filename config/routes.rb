aaba050ef97098315f366bf196428a8a2a256fa1
Catherine Cordova <ccordova41@gmail.com>
2017-09-12 05:55:02 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'user/home'
 
+root "static#home"
+
+ get '/signin' => 'sessions#new'
+ post '/signin' => 'sessions#create'
+ get "/signout" => "sessions#destroy"
+ post '/rides' => "rides#new"
+
+ resources :users
+ resources :attractions
 end
